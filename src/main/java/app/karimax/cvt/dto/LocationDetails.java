e85b909030f89f915977eb63482e921d561ab0c7
kelvinmaxwell <kelvinmaxwell82@gmail.com>
2024-02-07 16:37:37 +0300

    test changes

--- src/main/java/app/karimax/cvt/dto/LocationDetails.java
+++ src/main/java/app/karimax/cvt/dto/LocationDetails.java
@@ -1,31 +1,12 @@
 package app.karimax.cvt.dto;
 
+import lombok.Data;
+
+@Data
 public class LocationDetails {
-    String name;
+    String formatted_address;
     double latitude;
     double longitude;
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
 }
