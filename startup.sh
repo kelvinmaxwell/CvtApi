6b677693a1b1ffc2cf507f4e5a9f0fcf3b2771f2
kelvinmaxwell <kelvinmaxwell82@gmail.com>
2025-06-08 19:53:26 +0300

    fix : add client

--- startup.sh
+++ startup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Pull the latest images
-docker compose pull
-
+docker  pull kelvinmaxwell/cvtserver-springboot-images:latest
+docker  pull kelvinmaxwell/gateway-springboot-images:latest
+docker  pull kelvinmaxwell/cvtserver-springboot-images
+docker  pull kelvinmaxwell/towingambulance-springboot-images:latest
 # Start the services
 docker compose up -d
 
